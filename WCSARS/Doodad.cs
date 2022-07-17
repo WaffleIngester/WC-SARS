376909f9365c7027ff618a134b492a0f57e31a14
Billybob <billybobfatherson@gmail.com>
2022-07-17 16:47:12 -0700

    - Removed OffsetCollisionPoints2 property and combined those points into one variable "OffsetCollisionPoints"

--- WCSARS/Doodad.cs
+++ WCSARS/Doodad.cs
@@ -5,8 +5,7 @@ namespace WCSARS
     public class Doodad
     {
         public DoodadType DoodadType;
-        public List<Int32Point> OffsetCollisionPoints;
-        public List<Int32Point> OffsetCollisionPoints2; // working title
+        public List<Int32Point> OffsetCollisionPoints; // every CollisionPoint that this Doodad instance has. Move Collisions + Move-Sight Collisions
         public float X;
         public float Y;
 
@@ -27,16 +26,22 @@ namespace WCSARS
                     OffsetCollisionPoints.Add( new Int32Point((int)X + _instancePoint.x, (int)Y + _instancePoint.y) );
                 }
             }
-            List<Int32Point> _moveCollisionPts2 = DoodadType.MoveSightCollisionPoints;
-            if (_moveCollisionPts2 != null && _moveCollisionPts2.Count >= 1)
+            List<Int32Point> _moveSightCollisionPoints = DoodadType.MoveSightCollisionPoints;
+            if (_moveSightCollisionPoints != null && _moveSightCollisionPoints.Count >= 1)
             {
-                OffsetCollisionPoints2 = new List<Int32Point>();
-                int points = _moveCollisionPts2.Count;
+                /* If DoodadType.MoveCollisionPoints is null, that means there are NO MoveCollisionPoints for this Doodad. (also if List.Count = 0)
+                 * This means thate OffsetCollisionPoints is never initialized in the following method... so we have to do it here
+                 */
+                if (OffsetCollisionPoints == null)
+                {
+                    OffsetCollisionPoints = new List<Int32Point>();
+                }
+                int points = _moveSightCollisionPoints.Count;
                 Int32Point _instancePoint;
                 for (int i = 0; i < points; i++)
                 {
-                    _instancePoint = _moveCollisionPts2[i];
-                    OffsetCollisionPoints2.Add(new Int32Point((int)X + _instancePoint.x, (int)Y + _instancePoint.y));
+                    _instancePoint = _moveSightCollisionPoints[i];
+                    OffsetCollisionPoints.Add(new Int32Point((int)X + _instancePoint.x, (int)Y + _instancePoint.y));
                 }
             }
         }
