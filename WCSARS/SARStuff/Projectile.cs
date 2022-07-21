491edd911bbb1cfe9b394fef148f23d0d1c78efe
Billybob <billybobfatherson@gmail.com>
2022-07-20 21:19:44 -0700

    - removed weird class that was in this section + comments

--- WCSARS/SARStuff/Projectile.cs
+++ WCSARS/SARStuff/Projectile.cs
@@ -4,15 +4,27 @@ using System.Text;
 
 namespace SuperAnimalRoyale.Types
 {
-    internal class Projectile_
-    {
-    }
     public struct Projectile
     {
+        /// <summary>
+        /// The ID-Index of the Weapon used to create this Projectile.
+        /// </summary>
         public int WeaponID { get; private set; } // WeaponID == WeaponJSONIndex
+        /// <summary>
+        /// The Rarity of the Weapon which was used to craete this Projectile.
+        /// </summary>
         public int WeaponRarity { get; private set; }
+        /// <summary>
+        /// The SpawnX of this Projectile.
+        /// </summary>
         public float OriginX { get; private set; }
+        /// <summary>
+        /// The SpawnY of this Projectile.
+        /// </summary>
         public float OriginY { get; private set; }
+        /// <summary>
+        /// The Launch Angle of this Projectile.
+        /// </summary>
         public float Angle { get; private set; }
 
         public Projectile(int _id, int _rarity, float _oX, float _oY, float _angle)
